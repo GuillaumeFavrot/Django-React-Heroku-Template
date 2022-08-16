1619cb5528341c409f807874fc843f6e413d1c44
Guillaume <gui.favrot@gmail.com>
2022-08-16 20:04:19 +0200

    test

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['blahblahblooh.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['blahblahblooh.herokuapp.com','127.0.0.1']
 
 
 # Application definition
